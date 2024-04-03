fb4aa960e6fb86593dd322f9992b81f125fb2256
Adithya <jai@xpayback.com>
2024-04-03 10:59:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Components/home";
+import About from "./Components/about";
+import Work from "./Components/work";
+import Testimonial from "./Components/testimonial";
+import Contact from "./Components/contact";
+import Footer from "./Components/footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <About />
+      <Work/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
